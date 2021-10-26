d84383a34045e9dfb4ff70153801aac0949cd217
in2dam-b <in2dam-b@elorrieta.int>
2021-10-26 17:50:00 +0200

    Icono en el main

--- Agenda/app/src/main/java/com/example/agenda/MainActivity.java
+++ Agenda/app/src/main/java/com/example/agenda/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.agenda;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
