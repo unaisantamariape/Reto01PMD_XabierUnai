ef5732b5a6015a50acf07fff96ddec1e99a66199
in2dam-b <in2dam-b@elorrieta.int>
2021-10-27 18:49:19 +0200

    imagenes baseActivity definitivas

--- Agenda/app/src/main/java/com/example/agenda/BaseActivity.java
+++ Agenda/app/src/main/java/com/example/agenda/BaseActivity.java
@@ -1,4 +1,19 @@
 package com.example.agenda;
 
-public class BaseActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class BaseActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base);
+
+
+
+    }
 }
