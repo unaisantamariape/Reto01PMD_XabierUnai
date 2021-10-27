0a9d34803d001abfb09412fd0de23aaf1a5ff83d
in2dam-b <in2dam-b@elorrieta.int>
2021-10-27 19:01:26 +0200

    activity base declarada en el manifest

--- Agenda/app/src/main/java/com/example/agenda/BaseActivity.java
+++ Agenda/app/src/main/java/com/example/agenda/BaseActivity.java
@@ -5,15 +5,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class BaseActivity extends AppCompatActivity{
+    private ImageButton botonCrear = null;
+    private ImageButton botonVerLista = null;
+    private ImageButton botonVolver = null;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
 
+        botonCrear = (ImageButton) findViewById(R.id.idImageButtonNuevaTarea);
 
+
+        botonVerLista = (ImageButton) findViewById(R.id.idImageButtonVerTareas);
+
+
+        botonVolver = (ImageButton) findViewById(R.id.idImageButtonVolver);
 
     }
 }
