97b5bff576c03d405f629393aed13241f3ad5798
in2dam-b <in2dam-b@elorrieta.int>
2021-11-02 16:47:48 +0100

    Ir a los detalles de cada tarea

--- Agenda/app/src/main/java/com/example/agenda/ListActivity.java
+++ Agenda/app/src/main/java/com/example/agenda/ListActivity.java
@@ -2,10 +2,13 @@ package com.example.agenda;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
@@ -36,27 +39,38 @@ public class ListActivity extends AppCompatActivity {
 
         botonRealizadas = (Button) findViewById(R.id.idBtnTareasRealizadas);
         botonRealizadas.setOnClickListener(view -> {
+            view.setEnabled(false);
+            findViewById(R.id.idBtnTareasPendientes).setEnabled(true);
             tareas = dbManager.selectNombresRealizados();
             adapter = new ArrayAdapter<String>(ListActivity.this, R.layout.activity_adapter,
                     R.id.idTextViewTareasDB, tareas);
             listView.setAdapter(adapter);
-
-            view.setEnabled(false);
-            findViewById(R.id.idBtnTareasPendientes).setEnabled(true);
         });
 
         botonPendientes = (Button) findViewById(R.id.idBtnTareasPendientes);
         botonPendientes.setOnClickListener(view -> {
             view.setEnabled(false);
             findViewById(R.id.idBtnTareasRealizadas).setEnabled(true);
+            tareas = dbManager.selectNombresPendientes();
+            adapter = new ArrayAdapter<String>(ListActivity.this, R.layout.activity_adapter,
+                    R.id.idTextViewTareasDB, tareas);
+            listView.setAdapter(adapter);
         });
 
+        //De primeras enseña las que están pendientes
         tareas = dbManager.selectNombresPendientes();
 
         adapter = new ArrayAdapter<String>(ListActivity.this, R.layout.activity_adapter,
                 R.id.idTextViewTareasDB, tareas);
 
         listView = (ListView) findViewById(R.id.idListViewTareas);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(ListActivity.this,DetailActivity.class);
+                startActivity(intent);
+            }
+        });
         listView.setAdapter(adapter);
     }
 }
